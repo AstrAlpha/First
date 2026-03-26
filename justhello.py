6995e56a099d2c907a791b7476481c3c36699a90
AstrAlpha <omegastra2010@gmail.com>
2026-03-27 00:49:37 +0530

    Create justhello.py for greeting functionality
    Add a simple hello world program with user input.

---
+++ justhello.py
@@ -0,0 +1,12 @@
+def hello(to ="world"):
+    print("hello, ",to)
+hello()
+
+def main():
+    name = input("what's your name? ")
+    hello(name)
+
+#just hello to the world(carefull you gotta use the code after you define it doesnt implement on its own!!)...
+
+
+main()
